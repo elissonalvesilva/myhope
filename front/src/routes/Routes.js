646956e8fdbdc5fd9358eefa67044c3a5ca68b52
Elisson Alves Silva <nossilesilva@gmail.com>
2023-03-07 20:35:34 -0300

    feat: created quiz page

--- front/src/routes/Routes.js
+++ front/src/routes/Routes.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Login from '../pages/Login';
+import Quiz from "../pages/Quiz";
 import Ranking from "../pages/Ranking";
 import Signup from "../pages/Signup";
 import Wallet from "../pages/Wallet";
@@ -12,6 +13,7 @@ export default function AppRoutes() {
       <Route element={<Signup />} path="/signup" />
       <Route element={<Ranking />} path="/ranking" />
       <Route element={<Wallet />} path="/wallet" />
+      <Route element={<Quiz />} path="/quiz" />
     </Routes>
   )
 }
