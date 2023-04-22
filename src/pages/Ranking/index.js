7e8159ceb1ec7d8c4bba364882a2fc14e8235554
Elisson Silva <nossilesilva@gmail.com>
2023-04-22 01:01:20 -0300

    feat: created ranking page

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -8,21 +8,22 @@ import {
   Content,
   Header,
   Info,
-  Champions,
-  Champion,
   CurrentRank,
   LoadingMore,
 } from "./style";
 
-import crown from '../../assets/crown.gif';
-import user2 from '../../assets/user2.jpg';
-import user3 from '../../assets/user3.jpg';
 import UserList from '../../components/UserList';
 import { UserContext } from '../../contexts/UserContext';
 import { AuthenticationContext } from '../../contexts/AuthenticationContext';
 import { configEnv } from '../../config';
 import Loading from '../../components/Loading';
+import {
+  Champions,
+  Champion,
+} from "./style";
 
+import crown from '../../assets/crown.gif';
+import user2 from '../../assets/user2.jpg';
 
 export default function Ranking() {
   const [user, storeUser] = useContext(UserContext);
@@ -36,10 +37,11 @@ export default function Ranking() {
   };
   const { ref, inView } = useInView(options);
   const [isRenderAll, setIsRenderAll] = useState(false);
+  const [isInitial, setIsInitial] = useState(false);
   const limit = 2;
   const [isLoading, setIsLoading] = useState(true);
 
-  const sendQuery = useCallback(async () => {
+  const sendQuery = async () => {
     try {
       setIsLoading(true);
       const { data } = await axios.post(`${configEnv.MYHOPE_API}/user/ranking`, {
@@ -53,26 +55,25 @@ export default function Ranking() {
         setIsRenderAll(true);
       }
       setCurrentPage(data.page + 1);
-      
+      setIsInitial(true);
       setUsersRanking((prev) => [...prev, ...data.users]);
       setIsLoading(false);
     } catch (err) {
       console.log(err)
     }
-  }, [currentPage]);
+  };
 
   useEffect(() => {
-    console.log(inView, isRenderAll);
-    if(inView && isRenderAll === false) {
+    if(inView && !isRenderAll && isInitial) {
       sendQuery();
     }
   }, [inView])
 
   useEffect(() => {
-    if(!isRenderAll) {
+    if(!isInitial && isLoading) {
       sendQuery();
     }
-  }, [])
+  }, [isInitial, isLoading])
 
   return(
     <>
@@ -91,31 +92,38 @@ export default function Ranking() {
           </Info>
         </Header>
         <Champions>
-          <Champion className='second'>
-            <img src={user2} className='user-avatar' alt='user avatar' />
-            <span className='place'>2</span>
-            <span className='name'>Roberta Henriksen</span>
-          </Champion>
-          <Champion className='first'>
-            <div className='crown'>
-              <img src={crown} className='crown-winner' alt='crown' />
-            </div>
-            <img src={user} className='user-avatar' alt='user avatar' />
-            <span className='place'>1</span>
-            <span className='name'>Jhon Doe</span>
-          </Champion>
-          <Champion className='third'>
-            <img src={user3} className='user-avatar' alt='user avatar' />
-            <span className='place'>3</span>
-            <span className='name'>Desirae Davis</span>
-          </Champion>
+          {
+            userRanking.slice(0, 2).map((user, index) => {
+
+              const className = {
+                1: 'first',
+                2: 'second',
+                3: 'third',
+              }[user.position]
+
+              return (
+                <Champion className={className} key={index}>
+                  {
+                    user.position === 1 ? (
+                      <div className='crown'>
+                        <img src={crown} className='crown-winner' alt='crown' />
+                      </div>
+                    ) : null
+                  }
+                  <img src={user2} className='user-avatar' alt='user avatar' />
+                  <span className='place'>{user.position}</span>
+                  <span className='name'>{user.name}</span>
+                </Champion>
+              )
+            })
+          }
         </Champions>
         <CurrentRank>
           <h2>Sua posição atual</h2>
-          <span className='position'>1</span>
+          <span className='position'>{user.position}</span>
         </CurrentRank>
         <UserList users={userRanking} reference={null}/>
-        <LoadingMore ref={ref}>Carregando mais episodios...</LoadingMore>
+        <LoadingMore ref={ref}>Carregando mais usuarios...</LoadingMore>
       </Content>
     </Container>
     </>
